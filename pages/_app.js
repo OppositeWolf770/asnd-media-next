65ef742c650da5ed5b0f876152b816c837aac906
OppositeWolf770 <shschool42@gmail.com>
2023-07-24 16:39:26 -0500

    Removed React Buddy imports

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,7 @@
 import '@/styles/globals.sass'
 import Layout from "@/components/Layout";
-import {DevSupport} from "@react-buddy/ide-toolbox-next";
-import {ComponentPreviews, useInitial} from "@/components/dev";
 
-export default function App({Component, pageProps}) {
+export default function App({ Component, pageProps }) {
     return (
         <Layout>
             <Component {...pageProps} />
