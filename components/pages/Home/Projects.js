aa1ad3ff4a6131a3c8961c140fa933542ce67011
OppositeWolf770 <shschool42@gmail.com>
2023-07-24 23:29:46 -0500

    Added spacing from Prettier

--- components/pages/Home/Projects.js
+++ components/pages/Home/Projects.js
@@ -1,14 +1,14 @@
 import ProjectItem from "@/components/pages/Home/ProjectItem";
 
 import styles from '../../../styles/components/Home/projects.module.sass'
-import {projectItems} from "@/components/data/projectItems";
+import { projectItems } from "@/components/data/projectItems";
 
 export default function Projects() {
     return (
         <>
             <div className={styles.projects}>
                 {projectItems.map((projectItem) => (
-                    <ProjectItem img={projectItem.img} name={projectItem.name} key={projectItem.id}/>
+                    <ProjectItem img={projectItem.img} name={projectItem.name} key={projectItem.id} />
                 ))}
             </div>
         </>
