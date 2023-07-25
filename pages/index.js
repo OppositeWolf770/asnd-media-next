e2bc288b677ef2b93540d1c587db16cefcee80d6
OppositeWolf770 <shschool42@gmail.com>
2023-07-24 23:30:00 -0500

    Removed unused imports and constants.

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,8 @@
 import { Inter } from 'next/font/google'
-import Layout from "@/components/Layout";
-import Navbar from "@/components/Navbar";
 import Head from "next/head";
 import styles from '../styles/pages/home.module.sass'
 import Link from "next/link";
 import Projects from "@/components/pages/Home/Projects";
-
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
@@ -17,9 +12,9 @@ export default function Home() {
       </Head>
 
       <div className={`contentBox ${styles.content}`}>
-          <div className={styles.header}><span>ASND</span> Your Business to New Heights.</div>
-          <div className={styles.subheader}>With a wide variety of media options, find something that&apos;s right for you.</div>
-          <Link href={"/packages"}><button className={styles.button}>Boost Packages</button></Link>
+        <div className={styles.header}><span>ASND</span> Your Business to New Heights.</div>
+        <div className={styles.subheader}>With a wide variety of media options, find something that&apos;s right for you.</div>
+        <Link href={"/packages"}><button className={styles.button}>Boost Packages</button></Link>
       </div>
 
       <Projects />
