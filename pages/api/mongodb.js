1a2ef3b4f5371128d567884d2f1a47cd6c2e43cb
Daniel Hoelzeman <dhoelzeman@outlook.com>
2023-08-01 16:37:47 -0500

    Added some test information to test the MongoDB database.

--- pages/api/mongodb.js
+++ pages/api/mongodb.js
@@ -1,20 +1,40 @@
-const { MongoClient } = require("mongodb");
+import { MongoClient } from "mongodb";
+
+const ObjectId = require("mongodb").ObjectId;
+
+// const { MongoClient } = require("mongodb");
 
 export default async function GET(request, response) {
+  // URI found in the Environmental Variables
   const uri = process.env.MONGODB_URI;
+
+  // New instance of MongoDB
   const client = new MongoClient(uri);
 
-  let movie;
+  // Database and Collection names
+  const dbName = "main";
+  const collectionName = "projectItems";
+
+  let info;
 
   try {
-    const database = client.db("main");
-    const slides = database.collection("slides");
+    // Use connect method to connect to the server
+    await client.connect();
+    console.log("Successfully connected to the server");
+    const db = client.db(dbName);
+    const collection = db.collection(collectionName);
 
-    const query = { id: "1" };
+    info = collection.find({
+      id: {
+        $in: [1, 2],
+      },
+    });
 
-    movie = await slides.findOne(query);
+    // const query = { id: "1" };
+
+    // info = await collection.findOne(query);
   } catch (e) {
-    movie = e.message;
+    info = e.message;
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
@@ -22,7 +42,7 @@ export default async function GET(request, response) {
 
   return response.json(
     {
-      body: movie,
+      body: info,
     },
     {
       status: 200,
