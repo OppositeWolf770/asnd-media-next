db1994433b49aeaea7a66a5e0d2716d139c0591e
OppositeWolf770 <shschool42@gmail.com>
2023-08-15 20:37:49 -0500

    [TEMP] added a contentBox around the Project Items.

--- src/app/home/ProjectItem.jsx
+++ src/app/home/ProjectItem.jsx
@@ -5,15 +5,17 @@ import styles from "./projectItem.module.scss";
 export default function ProjectItem({ img, name }) {
   return (
     <>
-      <div className={styles.projectItem}>
-        <Image
-          src={img}
-          alt={""}
-          width={350}
-          height={250}
-          className={styles.image}
-        />
-        <div className={styles.name}>{name}</div>
+      <div className={`contentBox ${styles.projectBox}`}>
+        <div className={styles.projectItem}>
+          <Image
+            src={img}
+            alt={""}
+            width={350}
+            height={250}
+            className={styles.image}
+          />
+          <div className={styles.name}>{name}</div>
+        </div>
       </div>
     </>
   );
