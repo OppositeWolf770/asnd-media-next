b059f05ff697ac98f6b91623ac99c69ab8dd508b
OppositeWolf770 <shschool42@gmail.com>
2023-08-15 20:37:05 -0500

    Added metadata and removed the <Head> component.
    Removed the createClient from this component and moved it to the index page.jsx.

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -2,22 +2,17 @@ import Head from "next/head";
 import Slideshow from "./Slideshow";
 
 import styles from "./portfolio.module.scss";
+import { supabase } from "@/src/app/page";
 
-import { createClient } from "@supabase/supabase-js";
+export const metadata = {
+  title: "ASND Media - Portfolio",
+};
 
 export default async function Portfolio() {
-  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-  const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-
-  const supabase = createClient(supabaseUrl, supabaseKey);
-
   const { data: slides } = await supabase.from("slides").select().order("id");
 
   return (
     <>
-      <Head>
-        <title>ASND Media - Portfolio</title>
-      </Head>
       <div className={`contentBox ${styles.content}`}>
         Some Examples of our Work
       </div>
