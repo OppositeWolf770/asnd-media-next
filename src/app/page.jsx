ecb28d9e49058576aff6b57e75c3601b12a3673f
OppositeWolf770 <shschool42@gmail.com>
2023-08-15 20:37:22 -0500

    Added a universal createClient for supabase.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,4 +1,10 @@
 import { redirect } from "next/navigation";
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default async function Index() {
   redirect("/home");
