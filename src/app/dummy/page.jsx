97cd43a1c041809fe52662255f32f5f6940ad8d1
OppositeWolf770 <shschool42@gmail.com>
2023-08-15 20:35:12 -0500

    [TEMP] Added functionality to the dummy component page.

--- src/app/dummy/page.jsx
+++ src/app/dummy/page.jsx
@@ -1,9 +1,27 @@
-import Loading from "@/src/app/loading";
+"use client";
 
-export default function Page() {
+import Loading from "@/src/app/loading";
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default async function Page() {
+  const { data, error } = await supabase.storage.listBuckets();
+  console.log(data);
+
   return (
     <>
-      <Loading />
+      {/*{data.map((bucket) =>*/}
+      {/*  // <div>{bucket.created_at}</div>*/}
+      {/*  console.log(bucket)*/}
+      {/*)}*/}
+      <button>Create Bucket</button>
+      <div>
+        {data ? <div>Data is not null.</div> : <div>Data is null.</div>}
+      </div>
     </>
   );
 }
